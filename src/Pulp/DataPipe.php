ae8c70c93251e77259ac172f3201b8de29d40a6c
Mark Kimsal <metrofindings@gmail.com>
2018-01-14 13:42:44 -0500

    Add end callback to datapipe as second param

--- src/Pulp/DataPipe.php
+++ src/Pulp/DataPipe.php
@@ -10,16 +10,27 @@ class DataPipe implements \React\Stream\DuplexStreamInterface {
 	use \Evenement\EventEmitterTrait;
 
 	public $writeCallback;
+	public $endCallback;
 	public $closed = FALSE;
 
-	public function __construct($writeCallable) {
+	public function __construct($writeCallable=NULL, $endCallable=NULL) {
 		$this->writeCallback = $writeCallable;
+		$this->endCallback   = $endCallable;
+	}
+
+	public function log($msg, $params=array()) {
+		$this->emit('log', [$msg, $params]);
 	}
 
 	public function pause() {
 	}
 
 	public function end($data=null) {
+		$cb = $this->endCallback;
+		if ($cb) {
+			$cb($this);
+		}
+
 		$this->close();
 		$this->emit('end');
 	}
@@ -40,15 +51,17 @@ class DataPipe implements \React\Stream\DuplexStreamInterface {
 
 	public function write($data) {
 		$cb = $this->writeCallback;
-		$cb($data);
+		if ($cb) {
+			$cb($data, $this);
+		}
 	}
 
     public function pipe(WritableStreamInterface $dest, array $options = array())
     {
 		//bubble up log events until we get to something
 		//that is connected to main Pulp object and can output
-		$dest->on('log', function($data) {
-			$this->emit('log', [$data]);
+		$dest->on('log', function($data, $params=NULL) {
+			$this->emit('log', [$data, $params]);
 		});
 
         return Util::pipe($this, $dest, $options);
