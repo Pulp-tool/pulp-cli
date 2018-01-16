f0f693dad4c0bf19eb86a016e16b945f51a22291
Mark Kimsal <metrofindings@gmail.com>
2018-01-16 07:23:06 -0500

    Verbose Debug flag

--- src/Pulp/Debug.php
+++ src/Pulp/Debug.php
@@ -9,11 +9,22 @@ use Evenement\EventEmitterTrait;
 
 class Debug extends DataPipe {
 
-	public function __construct() {
+	public $verbose = FALSE;
+
+	public function __construct($opts=[]) {
+		if (array_key_exists('verbose', $opts)) {
+			$this->verbose = $opts['verbose'];
+		}
+
 	}
 
 	public function write($data) {
 		$this->emit('log', ['pulp-debug: <file>'.$data->getFilename().'</>']);
+		if ($this->verbose) {
+			$this->emit('log', ['      base: <file>'.$data->base.'</>']);
+			$this->emit('log', ['  pathname: <file>'.$data->getPathname().'</>']);
+			$this->emit('log', ['   partial: <file>'.$data->getPartialPathname().'</>']);
+		}
 		$this->emit('data', [$data]);
 	}
 }
