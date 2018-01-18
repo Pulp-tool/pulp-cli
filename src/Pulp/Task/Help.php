905cd6ab5583a9ab1125ee4e5e6ae5c9f376c372
Mark Kimsal <metrofindings@gmail.com>
2018-01-18 10:28:46 -0500

    Add some ascii art

--- src/Pulp/Task/Help.php
+++ src/Pulp/Task/Help.php
@@ -4,6 +4,19 @@ namespace Pulp\Task;
 class Help extends \Pulp\Task {
 
 	public function __invoke($pulp) {
+
+		$this->output("
+
+(_)(_)(_)(_)_                (_)(_)                     
+(_)        (_) _         _      (_)    _  _  _  _       
+(_) _  _  _(_)(_)       (_)     (_)   (_)(_)(_)(_)_     
+(_)(_)(_)(_)  (_)       (_)     (_)   (_)        (_)    
+(_)           (_)       (_)     (_)   (_)        (_)    
+(_)           (_)_  _  _(_)_  _ (_) _ (_) _  _  _(_)    
+(_)             (_)(_)(_) (_)(_)(_)(_)(_)(_)(_)(_)      
+                                      (_)               
+");
+
 		$this->output("Help: run php pulp.phar <name>{task}</>");
 		$this->output("define tasks in <file>.pulp/config.php</>");
 		$this->output("");
