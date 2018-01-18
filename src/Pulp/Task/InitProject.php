add5239c85d5771b1e9990f20f1963cc8086423f
Mark Kimsal <metrofindings@gmail.com>
2018-01-18 11:21:13 -0500

    Update sample config file

--- src/Pulp/Task/InitProject.php
+++ src/Pulp/Task/InitProject.php
@@ -4,26 +4,54 @@ namespace Pulp\Task;
 
 class InitProject extends \Pulp\Task {
 
-	public function __invoke() {
-	exec ('mkdir .pulp');
-	echo "Created directory .pulp\n";
+	public function __invoke($pulp) {
+		exec ('mkdir .pulp');
+		$pulp->log("Created directory <file>.pulp</>");
+		$sample = $this->getConfig();
+		file_put_contents('.pulp/config.php', $sample);
+		$pulp->log("Created file <file>.pulp/config.php</>");
+
+		$sample = $this->getComposer();
+		file_put_contents('.pulp/composer.json', $sample);
+		$pulp->log("Created file <file>.pulp/composer.json</>");
+		$pulp->log("Run <name>composer install</> from inside .pulp directory.");
+	}
+
+	public function getConfig() {
 $sample = <<<EOL
 <?php                                                                                                                                                                 
+use \Pulp\LiveReload as lr;
+use \Pulp\Less       as less;
 \$p = new \Pulp\Pulp();
 
-#\$p->task('build', ['log'], function() {
-#   echo "Building...\\n";
-#});
-#
-#\$t = time();
-#
-#\$p->task('log', function() use (\$t) {
-#   echo "Logging with closure variable: \$t...\\n";
-#});
-EOL;
-	file_put_contents('.pulp/config.php', $sample);
-	echo "Created file .pulp/config.php\n";
+\$watchDirsCode = ['src/**/*.php'];
+\$watchDirsLess = ['public/templates/**/*.less'];
+\$outputDirLess =  'public/templates/dist/css/';
 
+
+\$p->task('watch', function() use(\$p, \$watchDirsCode, \$watchDirsLess, \$outputDirLess) {
+	\$lr = new lr();
+	\$lr->listen(\$p->loop);
+
+	\$p->watch( \$watchDirsCode )->on('change', function(\$file) use (\$p) {
+		\$p->src(\$watchDirsCode)
+			->pipe(\$lr);
+	});
+
+	\$p->watch( \$watchDirsLess )->on('change', function(\$file) use (\$p, \$outputDirLess) {
+		\$p->src(\$watchDirsLess)
+			->pipe(new less( ['compress'=>TRUE] ))
+			->pipe(\$p->dest(\$outputDirLess))
+			->pipe(\$lr);
+	});
+});
+
+EOL;
+
+		return $sample;
+	}
+
+	public function getComposer() {
 $sample = <<<EOL
 {
     "repositories": [
@@ -42,8 +70,7 @@ $sample = <<<EOL
      }
 }
 EOL;
-	file_put_contents('.pulp/composer.json', $sample);
-	echo "Created file .pulp/composer.json\n";
-	echo "Run composer install from inside .pulp directory.\n";
+
+		return $sample;
 	}
 }
